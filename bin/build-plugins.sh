c799341e142d9b518397cdba1b25839a20ecdefc
kamataryo <kamataryo@users.noreply.github.com>
2020-11-01 09:46:41 +0900

    Add another plugin

--- bin/build-plugins.sh
+++ bin/build-plugins.sh
@@ -11,4 +11,12 @@ do
       "./public/$STAGE/fixed-map-plugin@$VERSION"
   done
 
+  VERSION=$(cat < ./node_modules/@geolonia/view-with-google-plugin/package.json | jq -r .version)
+  for VERSION in latest $VERSION
+  do
+    cp \
+      ./node_modules/@geolonia/view-with-google-plugin/dist/view-with-google-geolonia-plugin.min.js \
+      "./public/$STAGE/view-with-google-plugin@$VERSION"
+  done
+
 done
