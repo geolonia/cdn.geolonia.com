17ee1dcd3d383407c185348e5dc11059ad9b5412
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-02-22 05:02:23 +0900

    removed unused lib

--- bin/build-address.js
+++ bin/build-address.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const parse = require("csv-parse");
 const basePath = `${__dirname}/../public/address/japan`;
 const mkdirp = require("mkdirp");
-const { removeGun } = require('./lib/address')
 const path = require('path')
 
 const main = async () => {
--- bin/lib/address.js
+++
@@ -1,7 +0,0 @@
-module.exports.removeGun = (cityName) => {
-  let normalized = cityName
-  if (normalized.match(/^(.+[郡])(.+[町村].*)$/)) {
-      normalized = RegExp.$2
-  }
-  return normalized
-}
