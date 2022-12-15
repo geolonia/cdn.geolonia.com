41a6e46e6eae1ee50707f4c7012da45f3a48c5d5
Keitaroh Kobayashi <keita@kbys.me>
2022-12-15 17:05:10 +0900

    Update Embed API to v3

--- bin/build-embed.sh
+++ bin/build-embed.sh
@@ -3,10 +3,10 @@
 set -ex
 
 # dev (when dev is @geolonia/embed@next)
-npm remove @geolonia/embed2
-npm install @geolonia/embed2@npm:@geolonia/embed@next -D
-rsync -a node_modules/@geolonia/embed2/dist/ ./public/dev/
-mv ./public/dev/embed.js ./public/dev/embed
+# npm remove @geolonia/embed2
+# npm install @geolonia/embed2@npm:@geolonia/embed@next -D
+# rsync -a node_modules/@geolonia/embed2/dist/ ./public/dev/
+# mv ./public/dev/embed.js ./public/dev/embed
 
 npm remove @geolonia/embed
 npm install @geolonia/embed -D
@@ -16,5 +16,5 @@ rsync -a node_modules/@geolonia/embed/dist/ ./public/v1/
 mv ./public/v1/embed.js ./public/v1/embed
 
 # dev (when dev is the same as production @geolonia/embed)
-# rsync -a node_modules/@geolonia/embed/dist/ ./public/dev/
-# mv ./public/dev/embed.js ./public/dev/embed
+rsync -a node_modules/@geolonia/embed/dist/ ./public/dev/
+mv ./public/dev/embed.js ./public/dev/embed
