08f24610675d6f6e2b9e0f568bb5c055d7e9be2a
Keitaroh Kobayashi <keita@kbys.me>
2023-05-12 16:09:15 +0900

    Move alias support in to build-sprites & build-styles

--- bin/build-sprites.js
+++ bin/build-sprites.js
@@ -1,42 +1,49 @@
 #!/usr/bin/env node
 
-const fetch = require('node-fetch')
-const path = require('path')
-const fs = require('fs')
+const fetch = require('node-fetch');
+const path = require('node:path');
+const fs = require('node:fs');
 
-const baseUrl = 'https://geoloniamaps.github.io/'
+const baseUrl = 'https://geoloniamaps.github.io/';
 
-const spriteIds =[
+const spriteIds = [
   'basic',
   'basic-v1',
-  'gsi'
-]
+  'gsi',
+];
 
 const extensions = [
   '.json',
   '.png',
   '@2x.json',
-  '@2x.png'
-]
+  '@2x.png',
+];
 
-const baseDir = path.join(__dirname, '../public/sprites')
+const baseDir = path.join(__dirname, '../public/sprites');
 
-const buildSprite = async (style) => {
-
-  for (let i = 0; i < extensions.length; i++) {
-
-    const extension = extensions[i];
-    const file = `${style}${extension}`
-    const url = `${baseUrl}${style}/${file}`
-
-    const response = await fetch(url)
-    const outputDir = path.join(baseDir, file)
-
-    const data = await response.buffer()
-    fs.writeFileSync(outputDir, data)
+const buildSprite = async (style, tgtStyleName = undefined) => {
+  if (tgtStyleName === undefined) {
+    tgtStyleName = style;
   }
-}
+
+  for (const extension of extensions) {
+    const file = `${style}${extension}`;
+    const outputFile = `${tgtStyleName}${extension}`;
+    const url = `${baseUrl}${style}/${file}`;
+
+    const response = await fetch(url);
+    const outputPath = path.join(baseDir, outputFile);
+
+    const data = await response.buffer();
+    fs.writeFileSync(outputPath, data);
+  }
+};
 
 spriteIds.forEach((sprite) => {
-  buildSprite(sprite)
-})
+  buildSprite(sprite);
+
+  // build alias
+  if (sprite === 'basic-v1') {
+    buildSprite(sprite, 'basic-world');
+  }
+});
